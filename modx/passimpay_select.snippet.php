7ef978cd58ed7621f91cfe976c1efd17ab648d11
Passimpay <118345040+Passimpay@users.noreply.github.com>
2023-05-25 23:11:37 +0300

    Update passimpay_select.snippet.php

--- modx/passimpay_select.snippet.php
+++ modx/passimpay_select.snippet.php
@@ -1,5 +1,5 @@
 <?php
-include MODX_CORE_PATH . 'components/minishop2/custom/payment/mspassimpay.class.php';
+include_once MODX_CORE_PATH . 'components/minishop2/custom/payment/mspassimpay.class.php';
 $hand = new msPaspy();
 $hand->init($modx);
 return $hand->payment_fields();
